e4bc359b1bdf4eb08c1dcfddbd59d207cc4b935e
rafae <rafaeltheocc@gmail.com>
2023-10-07 12:46:04 -0300

    att3
    attt3

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/cabos')
+def cabos():
+    return render_template("cabos.html")
